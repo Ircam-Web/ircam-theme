0568d7ff94c6c4302a292212d1fcb32369a67ca3
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2018-11-13 18:27:26 +0100

    Fix package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import subprocess
 
-setup(name='ircam-www-theme',
+setup(name='ircam_www_theme',
     version='2.0',
     description='IRCAM theme for the Mezzo CMS',
     long_description='IRCAM theme for the Mezzo CMS',
